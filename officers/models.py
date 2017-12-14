41cc5a1348c3bb0e0bc7cff55c720c19c595b686
Akoh James <atadoga.akoh@gmail.com>
2017-12-14 17:53:31 +0100

    created model for Witness in witness app

--- officers/models.py
+++ officers/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-from accounts import models
+from accounts.models import User
 # Create your models here.
 
 
 class Officer(models.Model):
-    account = models.ForeignKey(models.User, related_name='officers')
+    account = models.ForeignKey(User, related_name='officers')
     id = models.CharField(primary_key=True, max_length=30, help_text='Enter unique Police id')
     activity = models.CharField(max_length=200)
