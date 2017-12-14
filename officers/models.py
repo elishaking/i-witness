cebb6296e065757215260897cc831318a46d1041
Akoh James <atadoga.akoh@gmail.com>
2017-12-14 16:45:50 +0100

    created Officer model for officers app

--- officers/models.py
+++ officers/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+from accounts import models
 # Create your models here.
+
+
+class Officer(models.Model):
+    user = models.ForeignKey(models.User, related_name='officers')
+    id = models.CharField(primary_key=True, max_length=30, help_text='Enter unique Police id')
+    activity = models.CharField(max_length=200)
