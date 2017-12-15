20c777a111e5ad92825648935ddc2acf0210c46f
elishaking <ek.chibueze@gmail.com>
2017-12-15 12:09:38 +0100

    updated witness serializer

--- witness/api/serializers.py
+++ witness/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from ..models import Witness
+from accounts.models import User
 from accounts.api.serializers import AccountsSerializer, AccountCreateSerializer
 from drf_writable_nested import WritableNestedModelSerializer
 
@@ -13,8 +14,6 @@ from django.db.models import Q
 #         model = Witness
 #         fields = ['account', 'reports']User_ = get_user_model()
 
-User_ = get_user_model()
-
 
 class WitnessSerializer(serializers.ModelSerializer):
     account = AccountsSerializer()
@@ -24,7 +23,7 @@ class WitnessSerializer(serializers.ModelSerializer):
         fields = ['account']
 
 
-class WitnessCreateSerializer(WritableNestedModelSerializer):
+class WitnessCreateSerializer(serializers.ModelSerializer):
     # email_confirm = serializers.EmailField(label='Confirm Email')
     account = AccountCreateSerializer()
 
@@ -32,45 +31,52 @@ class WitnessCreateSerializer(WritableNestedModelSerializer):
         model = Witness
         fields = ['account']
 
+    def create(self, validated_data):
+        account_data = validated_data.pop('account')
+        account = User.objects.create(**account_data)
+        witness = Witness.objects.create(account=account, **validated_data)
 
-class WitnessLoginSerializer(serializers.ModelSerializer):
-    token = serializers.CharField(allow_blank=True, read_only=True)
-    auth_field = serializers.CharField(label="Username or Email")
-    # email = serializers.EmailField()
+        return witness
 
-    class Meta:
-        model = Witness
-        fields = ['auth_field', 'password', 'token']
 
-        extra_kwargs = {
-            'password': {
-                'write_only': True
-            }
-        }
-
-    def validate(self, data):
-        username_or_email = data.get('auth_field', None)
-
-        if not username_or_email:
-            raise serializers.ValidationError('Enter valid Username or Email')
-
-        if str(username_or_email).find('@') != -1:
-            user = User_.objects.filter(Q(email=username_or_email)).distinct()
-        else:
-            user = User_.objects.filter(Q(username=username_or_email)).distinct()
-
-        # print(user)
-
-        if user.exists() and user.count() == 1:
-            user_obj = user[0]
-        else:
-            raise serializers.ValidationError('Incorrect Credentials, please try again')
-
-        if user_obj:
-            if not user_obj.check_password(raw_password=data['password']):
-                raise serializers.ValidationError("Incorrect Credentials, please try again")
-
-        return data
+# class WitnessLoginSerializer(serializers.ModelSerializer):
+#     token = serializers.CharField(allow_blank=True, read_only=True)
+#     auth_field = serializers.CharField(label="Username or Email")
+#     # email = serializers.EmailField()
+#
+#     class Meta:
+#         model = Witness
+#         fields = ['auth_field', 'password', 'token']
+#
+#         extra_kwargs = {
+#             'password': {
+#                 'write_only': True
+#             }
+#         }
+#
+#     def validate(self, data):
+#         username_or_email = data.get('auth_field', None)
+#
+#         if not username_or_email:
+#             raise serializers.ValidationError('Enter valid Username or Email')
+#
+#         if str(username_or_email).find('@') != -1:
+#             user = User_.objects.filter(Q(email=username_or_email)).distinct()
+#         else:
+#             user = User_.objects.filter(Q(username=username_or_email)).distinct()
+#
+#         # print(user)
+#
+#         if user.exists() and user.count() == 1:
+#             user_obj = user[0]
+#         else:
+#             raise serializers.ValidationError('Incorrect Credentials, please try again')
+#
+#         if user_obj:
+#             if not user_obj.check_password(raw_password=data['password']):
+#                 raise serializers.ValidationError("Incorrect Credentials, please try again")
+#
+#         return data
 
 
 """"""
