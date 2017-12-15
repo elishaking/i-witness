5c4237b15a76674dc0308e6d3f0ddd5ba018ba47
elishaking <ek.chibueze@gmail.com>
2017-12-15 12:15:13 +0100

    added account loginserializer

--- witness/api/serializers.py
+++ witness/api/serializers.py
@@ -39,44 +39,8 @@ class WitnessCreateSerializer(serializers.ModelSerializer):
         return witness
 
 
-# class WitnessLoginSerializer(serializers.ModelSerializer):
-#     token = serializers.CharField(allow_blank=True, read_only=True)
-#     auth_field = serializers.CharField(label="Username or Email")
-#     # email = serializers.EmailField()
-#
-#     class Meta:
-#         model = Witness
-#         fields = ['auth_field', 'password', 'token']
-#
-#         extra_kwargs = {
-#             'password': {
-#                 'write_only': True
-#             }
-#         }
-#
-#     def validate(self, data):
-#         username_or_email = data.get('auth_field', None)
-#
-#         if not username_or_email:
-#             raise serializers.ValidationError('Enter valid Username or Email')
-#
-#         if str(username_or_email).find('@') != -1:
-#             user = User_.objects.filter(Q(email=username_or_email)).distinct()
-#         else:
-#             user = User_.objects.filter(Q(username=username_or_email)).distinct()
-#
-#         # print(user)
-#
-#         if user.exists() and user.count() == 1:
-#             user_obj = user[0]
-#         else:
-#             raise serializers.ValidationError('Incorrect Credentials, please try again')
-#
-#         if user_obj:
-#             if not user_obj.check_password(raw_password=data['password']):
-#                 raise serializers.ValidationError("Incorrect Credentials, please try again")
-#
-#         return data
+class WitnessLoginSerializer(serializers.ModelSerializer):
+    pass
 
 
 """"""
