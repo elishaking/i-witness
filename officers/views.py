2c3df241c5ab40ee7714bf1174a6bf0d46e33b69
elishaking <ek.chibueze@gmail.com>
2018-01-28 21:41:45 +0100

    updated report template

--- officers/views.py
+++ officers/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+# from geopy.geocoders import Nominatim
+
+from reports.models import Report
+
 
 # Create your views here.
 def dashboard(request):
@@ -11,5 +15,14 @@ def profile(request):
 
 
 def reports(request):
-
-    return render(request, 'reports.html', {'first_name': 'King', 'last_name': 'Elisha'})
+    unresolved_reports = Report.objects.filter(resolved=False)
+    # loc = str(unresolved_reports[len(unresolved_reports) - 1].location).split(',')
+    # unresolved_reports[len(unresolved_reports) - 1].location = str(Nominatim().reverse('{0}, {1}'.format(loc[0], loc[1])))
+    resolved_reports = Report.objects.filter(resolved=True)
+    context = {
+        'first_name': 'King',
+        'last_name': 'Elisha',
+        'reports': unresolved_reports,
+        'resolved_reports': resolved_reports
+    }
+    return render(request, 'reports.html', context)
